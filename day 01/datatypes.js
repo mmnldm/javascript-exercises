0144aa63f9084f6911192a6edac37b50ecc0cfc1
mmnldm <mremmanueledem@gmail.com>
2023-01-11 14:34:26 +0100

    datatypes and variables exercise in console

---
+++ day 01/datatypes.js
@@ -0,0 +1,11 @@
+//Declare a variable and assign variables to each variable, use typeof operator and check the type of variable 
+
+let new_string = 'string datatype';
+let is_Bol = true;
+let new_Undefined;
+let un_known = null;
+
+console.log(typeof new_string);
+console.log(typeof is_Bol);
+console.log(typeof new_Undefined);
+console.log(typeof un_known);
---
+++ day 01/variable.js
@@ -0,0 +1,25 @@
+//Comments can make codes readable 
+//Welcome to 30 days of javascript 
+/*
+ Comments can make codes readable, easy to use and informative 
+ */
+
+
+//Declared variables and assigned datatypes to each of them
+let newString = 'string datatype';
+let isBol = true;
+let newUndefined;
+let unknown = null;
+
+console.log(newString, isBol, newUndefined, undefined);
+
+let unAssigned_1;
+let unAssigned_2;
+let unAssigned_3;
+let unAssigned_4;
+
+let Assigned_1 = "assigned first variable";
+let Assigned_2 = 2;
+let Assigned_3 = true;
+let Assigned_4 = `this is the fourth assigned variable`
+
