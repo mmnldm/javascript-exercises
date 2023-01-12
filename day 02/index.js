473d09395019fcc1f40008a2b3c03dd924a1f746
mmnldm <mremmanueledem@gmail.com>
2023-01-12 22:38:59 +0100

    day 2 exercises

---
+++ day 02/index.js
@@ -0,0 +1,14 @@
+let challenge = '30 Days of Javascript';
+let brands = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon';
+let challenge2 = "30 Days of Javascript";
+
+console.log(challenge);
+console.log(challenge.length);
+console.log(challenge.toUpperCase());
+console.log(challenge.toLowerCase());
+console.log(challenge.substring(3,21));
+console.log(challenge.substring(0,3));
+console.log(challenge.includes('script'));
+console.log(challenge.split(' '));
+console.log(brands.split(" "));
+console.log(challenge2.replace("Javascript", "Python"));
