7c3dfb9e9dea3d78b2ec1351a1ea3c8b8d812bb0
mmnldm <mremmanueledem@gmail.com>
2023-01-14 10:46:11 +0100

    more exercises answered

--- day 02/index.js
+++ day 02/index.js
@@ -52,6 +52,9 @@ console.log(phrase.includes("jargon"));
 let randomNumber = Math.floor(Math.random() * 101);
 console.log(randomNumber);
 
+let randomNumber2 = Math.floor(Math.random() * 256);
+console.log(randomNumber);
+
 
 let randomNum = Math.floor(Math.random() * 51 + 50);
 console.log(randomNum);
@@ -60,3 +63,19 @@ let myString = 'JavaScript';
 let randomIndex = Math.floor(Math.random() * myString.length);
 let randomChar = myString[randomIndex];
 console.log(randomChar);
+
+let min = 50;
+let max = 100;
+
+let randomnumber = Math.floor(Math.random() * (min - max) + min);
+console.log(randomnumber);
+
+let sliced = 'You cannot end a sentence with because because because is a conjunction';
+console.log(sliced.substring(31,55));
+
+let quote = 'Love is the best thing in this world. Some found their love and some are still looking for their love.'
+console.log(quote.match('love'));
+
+const sentence = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching';
+let newsentence = sentence.replace(/[^\w\s]/gi, '');
+console.log(newsentence);
