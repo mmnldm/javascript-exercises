244ea765833ad96a8d8d8ee38717c142eefaac1d
mmnldm <mremmanueledem@gmail.com>
2023-01-15 18:27:29 +0100

    question 1 answered

---
+++ day 03/index.js
@@ -0,0 +1,16 @@
+//1. 
+let firstName = "Emery";
+let lastName = "Edem";
+let country = "Nigeria";
+let city ="Calabar";
+let age = 23;
+let isMarried = true;
+let year = 2023;
+
+console.log(typeof(firstName));
+console.log(typeof(lastName));
+console.log(typeof(country));
+console.log(typeof(city));
+console.log(typeof(age));
+console.log(typeof(isMarried));
+console.log(typeof(year));
