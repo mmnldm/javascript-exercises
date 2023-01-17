0569a06d15b8527c45234b3bb97163b30ebee53c
mmnldm <mremmanueledem@gmail.com>
2023-01-17 19:39:44 +0100

    exercise 1

---
+++ day 04/index.js
@@ -0,0 +1,11 @@
+//E-L1 - 1
+let age  = prompt("Enter your age: ");
+
+if (age >= 18){
+console.log('You are old enough to drive')
+}
+else{
+    console.log(`You are left with ${18 - parseInt(age)} years to drive`)
+};
+
+// 2
