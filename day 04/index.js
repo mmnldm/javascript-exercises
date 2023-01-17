ab8c56b9f40fa8b0970d6feaf6246b035d3c4698
mmnldm <mremmanueledem@gmail.com>
2023-01-17 19:48:35 +0100

    exercise 2

--- day 04/index.js
+++ day 04/index.js
@@ -1,11 +1,21 @@
 //E-L1 - 1
-let age  = prompt("Enter your age: ");
+// let age  = prompt("Enter your age: ");
 
-if (age >= 18){
-console.log('You are old enough to drive')
-}
-else{
-    console.log(`You are left with ${18 - parseInt(age)} years to drive`)
-};
+// if (age >= 18){
+// console.log('You are old enough to drive')
+// }
+// else{
+//     console.log(`You are left with ${18 - parseInt(age)} years to drive`)
+// };
 
 // 2
+
+const myAge = 22;
+let yourAge = prompt('Enter your age: ');
+
+if (myAge >= yourAge){
+console.log('I am older than you');
+}
+else{
+    console.log(`You are ${parseInt(yourAge) - myAge } years older than me` );
+};
