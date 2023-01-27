e3c894b14bbe3681b12bc68104c94f0290fa2474
mmnldm <mremmanueledem@gmail.com>
2023-01-27 10:27:49 +0100

    conditional branching

--- conditional branching/main.js
+++ conditional branching/main.js
@@ -43,3 +43,6 @@ else{
 
 let result = ( a + b < 4) ? 'Below' : 'Over';
 
+// rewrite if else into multiple tenary operator
+
+let message = (login == 'Employee') ? 'Hello' : (login == 'Director') ? 'Greetings' : (login = '') ? 'No login' : '';
