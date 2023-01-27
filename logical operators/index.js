7ca7679b34413658494d29c7f962f7e10eb73239
mmnldm <mremmanueledem@gmail.com>
2023-01-27 10:41:40 +0100

    or operator

---
+++ logical operators/index.js
@@ -0,0 +1,12 @@
+let hour = 9;
+
+if(hour < 10 || hour > 18){
+    console.log("The office is closed");
+}
+
+// example of OR operator
+let firstname = '';
+let middlename = '';
+let lastname = '';
+
+console.log(firstname||middlename||lastname||'Namer');
