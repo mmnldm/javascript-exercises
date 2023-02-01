4be657c9b9d0117b74712d3e91fb1dd03406dc76
mmnldm <mremmanueledem@gmail.com>
2023-02-01 13:12:06 +0100

    commit changes

---
+++ while loop/main.js
@@ -0,0 +1,24 @@
+// let i = 0;
+// while( i < 10) {
+//     console.log(i);
+//     i += 2;
+// }
+
+let n = 0;
+let x = 0;
+
+while(n < 3){
+    n++;
+    x += n;
+    console.log(x);
+}
+
+let sum = 0;
+let i = 2;
+while (i <= 100) {
+  if (i % 2 === 0) {
+    sum += i;
+  }
+  i++;
+}
+console.log(sum);
