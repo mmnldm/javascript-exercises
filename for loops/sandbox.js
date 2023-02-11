1d18bd9a63a5d15634e4033806265d6b1214a75b
mmnldm <mremmanueledem@gmail.com>
2023-02-11 10:54:26 +0100

    understanding for loop

--- for loops/sandbox.js
+++ for loops/sandbox.js
@@ -1,5 +1,12 @@
 let log = console.log;
 
-for (let i = 0; i < 5; i++){
+for (let i = 1; i < 5; i++){
     log(i);
 }
+
+for (let even = 2; even < 11; even++){
+    if (even % 2 == 0){
+        console.log(even);
+    }
+}
+
