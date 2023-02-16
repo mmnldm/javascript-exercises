041a43821fe251cbaf4d76ee94f3c916dd7b7b6a
mmnldm <mremmanueledem>
2023-02-16 16:15:57 +0100

    continue statement

--- for loops/sandbox.js
+++ for loops/sandbox.js
@@ -4,9 +4,11 @@ for (let i = 1; i < 5; i++){
     log(i);
 }
 
-for (let even = 2; even < 11; even++){
-    if (even % 2 == 0){
-        console.log(even);
-    }
-}
+// * continue 
 
+for (i = 0; i < 10; i++){
+
+    if(i % 2 == 0) continue;
+    
+    alert(i);
+}
