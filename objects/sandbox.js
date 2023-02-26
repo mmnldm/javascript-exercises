31872363df2676ed3bddb57c348e0306819fd204
mmnldm <mremmanueledem@gmail.com>
2023-02-26 01:00:28 +0100

    solving tasks on objects in js

---
+++ objects/sandbox.js
@@ -0,0 +1,47 @@
+
+//* 
+
+function isEmpty(obj){
+   for(let key in obj){
+    
+    return false;
+   }
+    return true;
+    
+}
+console.log(isEmpty());
+
+
+// * Exercise 2 
+
+    let salaries = {
+        John: 100,
+        Ann: 160,
+        Pete: 130
+      }
+      
+      let sum = 0;
+      for(let key in salaries){
+        sum += salaries[key];
+      }
+
+      console.log(sum);
+
+// * Exercise 3 
+
+let menu = {
+    width: 200,
+    height: 300,
+    title: "My menu"
+  };
+
+  function multiplyNumeric(menu) {
+    for (let key in menu) {
+      if (typeof menu[key] == 'number') {
+        menu[key] *= 2;
+        return menu;
+      }
+    }
+  }
+console.log(multiplyNumeric(menu));
+console.log(multiplyNumeric(menu));
